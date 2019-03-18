7e3d3d5b1f68ea5ab641fddd0c2820ead4157e0c
Ariel Arnedo <ariel.arnedo@ceiba.com.co>
2019-03-17 20:25:53 -0500

    agragando las routas de los endpoints

---
+++ src/interfaces/http/routes/device.route.js
@@ -0,0 +1,33 @@
+'use-strict'
+
+//importar express
+var express = require('express');
+
+//cargar el Router
+var api = express.Router();
+
+//cargar el controlador
+
+
+//cargar el middleware de autenticacion
+
+
+//cargar si se requiere el connect-multiparty
+
+
+
+//cargar las rutas get
+
+
+//cargar las rutas post
+
+
+//cargar las rutas put
+
+
+//cargar las rutas delete
+
+
+
+//exportar la ruta
+module.exports = api;
